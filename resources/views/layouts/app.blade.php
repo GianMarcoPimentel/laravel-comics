412b1062b885bdafbba7debd8d57f9b56ae7cb55
GianMarcoPimentel <gianmarco.pimentel1997@hotmail.com>
2024-04-08 17:25:01 +0200

    creo cartella partials con dentro la nav e la inludo nella mia pagina dell'app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,17 +11,9 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
-    <nav>
-        <div class="container">
-
-            <div class="sites">
-                <span> dc power visa®</span>
-                <span>additional sites <i class="fa-solid fa-caret-down"></i></span>
-            </div>
-
-        </div>
-    </nav>
-        @yield('content')
+    
+    @include('partials/nav')
+    @yield('content')
     
 </body>
 </html>
---
+++ resources/views/partials/nav.blade.php
@@ -0,0 +1,10 @@
+<nav>
+    <div class="container">
+
+        <div class="sites">
+            <span> dc power visa®</span>
+            <span>additional sites <i class="fa-solid fa-caret-down"></i></span>
+        </div>
+
+    </div>
+</nav>
